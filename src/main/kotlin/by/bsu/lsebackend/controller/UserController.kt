a6d0ff0c89759410b6f0f55b3cbdec5da958ced9
k.ostapchuk <k.ostapchuk@itransition.com>
2022-09-04 18:54:47 +0300

    refactor: upd imports

--- src/main/kotlin/by/bsu/lsebackend/controller/UserController.kt
+++ src/main/kotlin/by/bsu/lsebackend/controller/UserController.kt
@@ -5,7 +5,15 @@ import by.bsu.lsebackend.dto.UserResponse
 import by.bsu.lsebackend.service.UserService
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.HttpStatus.NO_CONTENT
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import javax.validation.Valid
 
