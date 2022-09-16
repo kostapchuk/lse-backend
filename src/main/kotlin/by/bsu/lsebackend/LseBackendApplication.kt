2230acf93ff9c69190c93c658abab4ee260a1e6d
k.ostapchuk <k.ostapchuk@itransition.com>
2022-09-16 20:55:34 +0300

    refactor: remove todo

--- src/main/kotlin/by/bsu/lsebackend/LseBackendApplication.kt
+++ src/main/kotlin/by/bsu/lsebackend/LseBackendApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableAsync
 
-// todo add controller advice
 // todo change logic to have cost for answer not for question
 
 @SpringBootApplication
