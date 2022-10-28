6ca1dcec08f89230f6fdfb2153b973ffc29f956c
k.ostapchuk <k.ostapchuk@itransition.com>
2022-10-28 16:22:46 +0300

    fix: cache evict

--- src/main/kotlin/by/bsu/lsebackend/service/QuizService.kt
+++ src/main/kotlin/by/bsu/lsebackend/service/QuizService.kt
@@ -5,6 +5,7 @@ import by.bsu.lsebackend.dto.QuizResponse
 import by.bsu.lsebackend.entity.Quiz
 import by.bsu.lsebackend.mapper.QuizMapper
 import by.bsu.lsebackend.repository.QuizRepository
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
@@ -21,5 +22,6 @@ class QuizService(private val quizRepository: QuizRepository, private val quizMa
             .take(size)
             .map { quizMapper.toResponse(it) }
 
+    @CacheEvict("quizzes")
     fun create(quiz: QuizRequest): Mono<Quiz> = quizRepository.save(quizMapper.toEntity(quiz))
 }
